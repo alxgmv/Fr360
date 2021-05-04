39ed3b5e27e3125110602d9a547077641ee211d7
alxgmv <alxgmv@gmail.com>
2021-05-04 14:33:38 +0200

    1st

---
+++ data.js
@@ -0,0 +1,236 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-interyor_360_living",
+      "name": "Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.39206783589313865,
+          "pitch": -0.013867933070473626,
+          "rotation": 6.283185307179586,
+          "target": "1-interyor_360_hall"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6162255735684852,
+          "pitch": 0.31994674128837985,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/AWkuPLQI-H\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.0791865448517175,
+          "pitch": -0.11491738316548172,
+          "title": "AYR",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/at08dSJShg\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.6703411864058495,
+          "pitch": 0.12423339871738293,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/gi5cyeLYU\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.3959129633260723,
+          "pitch": 0.34354298122104154,
+          "title": "TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/1lTf5AS6EQ\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.8218697436375058,
+          "pitch": 0.8035333714625814,
+          "title": "SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/pH2PRxxyj\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.13887218292616055,
+          "pitch": 0.4227090749590374,
+          "title": "PRYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-pryme/7BJg53MJT\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 2.0298205056784484,
+          "pitch": 0.38914600317630743,
+          "title": "PYLLOW",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-pyllow/I9kvofOpF\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    },
+    {
+      "id": "1-interyor_360_hall",
+      "name": "Flur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6536474845378084,
+          "pitch": 0.12093957938355082,
+          "rotation": 0,
+          "target": "0-interyor_360_living"
+        },
+        {
+          "yaw": 0.13054907208305622,
+          "pitch": 0.17265781231642308,
+          "rotation": 0,
+          "target": "2-interyor_360_bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9964253568362604,
+          "pitch": -0.34416584438464604,
+          "title": "AYR",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/4AS1dcm1q\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -1.920066351958921,
+          "pitch": 0.4504208822474496,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/k7DPbI-8A\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.6492916464652225,
+          "pitch": 0.706994467794626,
+          "title": "SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/-JXqrwusm\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    },
+    {
+      "id": "2-interyor_360_bedroom",
+      "name": "Schlafzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.377411664011155,
+          "pitch": 0.05493059391694288,
+          "rotation": 0,
+          "target": "1-interyor_360_hall"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6304375521139356,
+          "pitch": 0.313481095551964,
+          "title": "SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/z-oUIU_ye\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 3.0580805138523175,
+          "pitch": 1.0233792952240854,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/OdPldood2\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.5371709606433477,
+          "pitch": 0.3905498037377324,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/dYGAhIn94\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.7637893981878552,
+          "pitch": 0.38205438259196356,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/dYGAhIn94\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 2.9393941255950278,
+          "pitch": 0.23099540139509145,
+          "title": "GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/qIUawidtx\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -2.450167190445896,
+          "pitch": 0.4689512082174314,
+          "title": "STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/QijteFUox\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.1440780495774856,
+          "pitch": -0.00015436030689741642,
+          "title": "LYNE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/kleiderschrank/rWnEO1ERW\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
